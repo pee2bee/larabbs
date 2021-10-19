492b12eaa8affa12c680ab0201ab57d0f61a9479
vagrant <vagrant@homestead>
2021-10-19 03:31:01 +0000

    基础页面结构

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -26,7 +26,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string|null
      */
-    // protected $namespace = 'App\\Http\\Controllers';
+    protected $namespace = 'App\\Http\\Controllers';// 取消这一行的注释
 
     /**
      * Define your route model bindings, pattern filters, etc.
