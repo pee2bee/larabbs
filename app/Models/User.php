bc6244ae4095837b02faf6747d056b10696ef38b
vagrant <vagrant@homestead>
2021-10-30 09:31:12 +0000

    消息通知有新回复

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
+use Illuminate\Support\Facades\Auth;
 
 /**
  * App\Models\User
@@ -25,8 +26,11 @@ use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
 class User extends Authenticatable implements MustVerifyEmailContract
 
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory,MustVerifyEmailTrait;
 
+    use Notifiable{
+        notify as protected laravelNotify;
+    }
     /**
      * The attributes that are mass assignable.
      *
@@ -77,6 +81,20 @@ class User extends Authenticatable implements MustVerifyEmailContract
     }
 
 
+    public function notify($instance)
+    {
+        // 如果要通知的人是当前用户，就不必通知了！
+        if($this->id == Auth::id()){
+            return;
+        }
+
+        //只有数据库类型通知才需提醒，直接发送 Email 或者其他的都 Pass
+        if(method_exists($instance,'toDatabase')){
+            $this->increment('notification_count');
+        }
+
+        $this->laravelNotify($instance);
+    }
 
 
 
