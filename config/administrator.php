1ca9aa44dcb47c1dde2374b352061ca605868df3
vagrant <vagrant@homestead>
2021-11-03 20:13:54 +0000

    内容管理后台

--- config/administrator.php
+++ config/administrator.php
@@ -68,6 +68,11 @@ return array(
             'roles',
             'permissions',
         ],
+        '内容管理' => [
+            'categories',
+            'topics',
+            'replies',
+        ],
     ],
 
     /*
