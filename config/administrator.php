7fe12ab9ad2322fc7fe37f3ccb58bf6f3829ddff
vagrant <vagrant@homestead>
2021-11-03 19:56:24 +0000

    管理员可以管理权限和角色

--- config/administrator.php
+++ config/administrator.php
@@ -42,27 +42,7 @@ return array(
     //配置信息文件存放目录
     'settings_config_path' => config_path('administrator/settings'),
 
-    /*
-     * The menu structure of the site. For models, you should either supply the name of a model config file or an array of names of model config
-     * files. The same applies to settings config files, except you must prepend 'settings.' to the settings config file name. You can also add
-     * custom pages by prepending a view path with 'page.'. By providing an array of names, you can group certain models or settings pages
-     * together. Each name needs to either have a config file in your model config path, settings config path with the same name, or a path to a
-     * fully-qualified Laravel view. So 'users' would require a 'users.php' file in your model config path, 'settings.site' would require a
-     * 'site.php' file in your settings config path, and 'page.foo.test' would require a 'test.php' or 'test.blade.php' file in a 'foo' directory
-     * inside your view directory.
-     *
-     * @type array
-     *
-     * 	array(
-     *		'E-Commerce' => array('collections', 'products', 'product_images', 'orders'),
-     *		'homepage_sliders',
-     *		'users',
-     *		'roles',
-     *		'colors',
-     *		'Settings' => array('settings.site', 'settings.ecommerce', 'settings.social'),
-     * 		'Analytics' => array('E-Commerce' => 'page.ecommerce.analytics'),
-     *	)
-     */
+
         /*
      * 后台菜单数组，多维数组渲染结果为多级嵌套菜单。
      *
@@ -85,6 +65,8 @@ return array(
     'menu' => [
         '用户与权限'=>[
             'users',
+            'roles',
+            'permissions',
         ],
     ],
 
